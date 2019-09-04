5c31fe2f214d8ae2160d7765fa49e4dbd2cc321f
Lazarocp <lazaro.cuenca8@gmail.com>
2019-09-04 18:20:16 +0200

    seguimos con ello

--- main.js
+++ main.js
@@ -2,10 +2,7 @@ const AutenticacioModule = require('./Programador Tasques/Tasques/Autenticacio')
 const AutoritzacioModule = require('./Programador Tasques/Tasques/Autorizacio')
 const TasquesModule = require('./Programador Tasques/Tasques')
 const vehicleModule = require('./Programador Tasques/Tasques/vehicle')
-
-let Autorizacio = new AutoritzacioModule.Autoritzacio();
-let Autenticacio = new AutenticacioModule.Autenticacio();
-let AutoritzacioTasca = TasquesModule.Tasca(Autorizacio);
-let AutenticacioTasca = TasquesModule.Tasca(Autenticacio);
+let AutoritzacioTasca = TasquesModule.Tasca(new AutoritzacioModule.Autoritzacio());
+let AutenticacioTasca = TasquesModule.Tasca(new AutenticacioModule.Autenticacio());
 
 
